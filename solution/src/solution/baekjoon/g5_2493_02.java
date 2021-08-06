402e03b15cd8c3166b315aad1cfa88e9efb0731d
boomini <bomin2641@gmail.com>
2021-08-06 17:34:44 +0900

    백준 2493 재업로드

--- solution/src/solution/baekjoon/g5_2493_02.java
+++ solution/src/solution/baekjoon/g5_2493_02.java
@@ -11,40 +11,28 @@ public class g5_2493_02 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
-		Stack<Integer> top = new Stack<>();
-		int[] input = new int[N];
+		Stack<int[]> top = new Stack<>();
 		String s= in.readLine();
-
 		StringTokenizer st = new StringTokenizer(s, " ");
-		StringBuilder sb = new StringBuilder();
-		int check = Integer.parseInt(st.nextToken());
-		int last = 0, compare =0;
-		
-		input[0]=check;
-		for(int i=1; i<N; i++) {
-			compare = Integer.parseInt(st.nextToken());
-			input[i] = compare;
-			if(check>=compare) top.add(check);
-			check = compare;
-		}
-		
-		int index = 0;
-		int count = 0;
-		int j=0;
-		while(true) {
-			int topNum = top.poll();
-			while(true) {
-				int num = input[j++];
-				System.out.print(index + " ");
-				count++;
-				if(j==N) break;
-				if(topNum == num ) {
-					index = count;
-					if(top.isEmpty()) continue;
-					break;
+		StringBuilder sb = new StringBuilder();	
+		for(int i=1; i<=N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			while(!top.isEmpty()) {
+				if(top.peek()[1]<num) {
+						top.pop();
 				}
+				else {
+					sb.append(top.peek()[0]).append(" ");
+					break;
+				}				
 			}
-			if(top.isEmpty()) break;
-		}		
+			
+			if(top.isEmpty()) sb.append(0).append(" ");
+			
+			top.add(new int[] {i,num});
+			
+		}
+		System.out.println(sb.toString());
 	}
 }
